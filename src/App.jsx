1b5d90ee2b7754af1baa760147fec16348ac4b76
Talha Imtiaz <talha@optiapm.com>
2025-07-07 13:30:16 +0500

    chore: add intentionally buggy and silly code to homepage for testing

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,24 @@ import ContactForm from "./components/ContactForm";
 import Footer from "./components/Footer";
 
 function App() {
+  // BAD: Unused variable
+  let x = 0;
+  // BAD: Inline style, typo, and silly bug
   return (
-    <div className="bg-gray-950 text-white min-h-screen font-sans">
+    <div className="bg-gray-950 text-white min-h-screen font-sans" style={{ bakgroundColor: 'hotpink', color: 'lime', fontSize: 99 }}>
+      <h1>Welcome to the Buggy Homepage!!!</h1>
+      <p>{window.doesNotExist.boom}</p> {/* BAD: Will throw error */}
+      <button onclick="alert('hi')">Click me (bad event handler)</button>
+      <div dangerouslySetInnerHTML={{ __html: '<img src=x onerror=alert(1) />' }} /> {/* BAD: XSS */}
+      {[1,2,3].map(i => <div key={Math.random()}>Duplicate keys!</div>)}
+      <footer>Copyright 2025</footer>
+      <div>Unclosed div</div>
       <Navbar />
       <main>
         <Hero />
         {/* <Demo /> */}
         <Features />
-        <AlertExample />
+          <AlertExample />      
         <BuiltFor />
         <Testimonials />
         <Integrations />
